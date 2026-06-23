285394e7b88d3b33627421209c6ae0cb4af65fea
Erik Petersen <eht@it-trans.dk>
2026-06-23 21:27:50 +0200

    Set custom base url for tests
    This will prevent access to the real web service while testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,5 @@ import adressevaelger
 
 @pytest.fixture(scope="package")
 def client() -> adressevaelger.Client:
-    return adressevaelger.Client()
+    return adressevaelger.Client(base_url="https://adressevaelger.local")
+
